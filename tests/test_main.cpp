defcacb66163aac586ee15dee487cfd08a3f3a11
User <muhammed.omayrat@gmx.de>
2026-08-09 13:46:41 +0200

    Minor visual fix

---
+++ tests/test_main.cpp
@@ -0,0 +1,12 @@
+/*!
+ *\file test_main.cpp
+ *\brief GoogleTest entry point with colored console output enabled.
+ */
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+   testing::InitGoogleTest(&argc, argv);
+   testing::GTEST_FLAG(color) = "yes";
+   return RUN_ALL_TESTS();
+}
